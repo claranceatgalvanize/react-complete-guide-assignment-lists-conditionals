1d321ca4e22f9ac8049b4f115101bc5789da9288
claranceatgalvanize <claranceatgalvanize@gmail.com>
2019-10-30 01:47:47 -0700

    all done

--- src/App.js
+++ src/App.js
@@ -5,83 +5,42 @@ import CharComponent from "./CharComponent";
 
 class App extends Component {
   state = {
-    textLength: 0,
     inputValue: ""
   };
-  inputLength = e => {
-    let text = e.target.value;
-    this.setState({ textLength: text.length, inputValue: text });
+  handleInput = e => {
+    this.setState({ inputValue: e.target.value });
   };
-  deleteChar = id => {
+  deleteChar = idx => {
     const chars = this.state.inputValue.split("");
-    chars.splice(id, 1);
-    this.setState({ inputValue: chars.join("") });
+    chars.splice(idx, 1);
+    const updatedInputValue = chars.join("");
+    this.setState({ inputValue: updatedInputValue });
   };
   render() {
-    const letters = this.state.inputValue.split("");
-    let tiles = null;
-    if (letters) {
-      tiles = (
-        <div>
-          {letters.map((char, idx) => {
-            return (
-              <CharComponent
-                deleteChar={this.deleteChar}
-                key={idx}
-                id={idx}
-                letter={char}
-              />
-            );
-          })}
-        </div>
+    let tiles = this.state.inputValue.split("").map((char, idx) => {
+      return (
+        <CharComponent
+          deleteChar={() => this.deleteChar(idx)}
+          key={idx}
+          letter={char}
+        />
       );
-    }
+    });
 
     return (
       <div className="App">
-        <ol>
-          <li>
-            [ √ ] Create an input field (in App component) with a change
-            listener which outputs the length of the entered text below it (e.g.
-            in a paragraph).
-          </li>
-          <li>
-            [ √ ] Create a new component (=> ValidationComponent) which receives
-            the text length as a prop
-          </li>
-          <li>
-            [ √ ] Inside the ValidationComponent, either output "Text too short"
-            or "Text long enough" depending on the text length (e.g. take 5 as a
-            minimum length)
-          </li>
-          <li>
-            [ √ ] Create another component (=> CharComponent) and style it as an
-            inline box (=> display: inline-block, padding: 16px, text-align:
-            center, margin: 16px, border: 1px solid black).
-          </li>
-          <li>
-            [ √ ] Render a list of CharComponents where each CharComponent
-            receives a different letter of the entered text (in the initial
-            input field) as a prop.
-          </li>
-          <li>
-            [ * ] When you click a CharComponent, it should be removed from the
-            entered text.
-          </li>
-        </ol>
         <h5>Enter a quote sixteen characters long.</h5>
         <input
-          onChange={this.inputLength}
+          onChange={this.handleInput}
           type="text"
-          placeholder="Eg: Start somewhere!"
           value={this.state.inputValue}
         />
         <br></br>
         <small>
-          You've entered {this.state.textLength}{" "}
-          {this.state.textLength <= 0 ? "character" : "characters"}
+          You've entered {this.state.inputValue.length}{" "}
+          {this.state.inputValue.length <= 0 ? "character" : "characters"}
         </small>
-        <ValidationComponent textLength={this.state.textLength} />
+        <ValidationComponent textLength={this.state.inputValue.length} />
         {tiles}
       </div>
     );
--- src/CharComponent.js
+++ src/CharComponent.js
@@ -1,17 +1,9 @@
 import React from "react";
+import "./CharComponent.css";
 
 const CharComponent = ({ letter, deleteChar, id }) => {
-  const style = {
-    display: "inline-block",
-    padding: "16px 0",
-    textAlign: "center",
-    margin: "1px",
-    width: "50px",
-    color: "#ccc",
-    background: "#111"
-  };
   return (
-    <div onClick={() => deleteChar(id)} style={style}>
+    <div className="tiles" onClick={() => deleteChar(id)}>
       {letter}
     </div>
   );
